3961b049ba9c7b741836d1a4be1259fe98073846
nathalanLemoine <106219@ecole-it.com>
2025-11-18 15:08:02 +0100

    occulus réparo

--- api/index.js
+++ api/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const client = require('prom-client');
-const cors = require('cors');
 
 const app = express();
-
-app.use(cors()); 
-
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
 const register = client.register;
 
 app.get('/', (req, res) => res.json({ message: 'API OK' }));
-
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
-
 app.get('/metrics', async (req, res) => {
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
